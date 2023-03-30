ff39cbad4cd3423e549661f68f3e0bc3d22f989c
Gabriel Sargent <gabe2sargent@gmail.com>
2023-03-30 06:20:58 -0500

    flow

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(someVaule){
+  if(someVaule < 400 ) {
+    return "This one is on me!"
+  }else if(someVaule > 400 && someVaule < 2000){
+   return "That will be twenty bucks."
+  }else if(someVaule  > 2000 && someVaule <= 2500 ){
+    return "I will gladly take your thirty bucks."
+  }else if(someVaule > 2500 ){
+    return "No can do."
+  }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function ternaryCheckCity(city){
+  if(city === 'NYC'){
+    return "Ok, sounds good."
+  }else{
+    return "No go."
+  }
+  }
+
+
+function switchOnCharmFromTip(reply){
+    switch(reply){
+    case 'generous':
+     return 'Thank you so much.'
+     case 'not as generous':
+      return 'Thank you.'
+      default:
+        return 'Bye.'
+
+
+  }
+
+
+
+
+
+
+
+  }
+
+
+
